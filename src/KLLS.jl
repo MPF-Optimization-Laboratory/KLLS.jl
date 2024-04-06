cac35add367d8389c6ecba904883007e45b96dd7
Michael Friedlander <michael@friedlander.io>
2024-04-05 23:17:33 -0700

    Local convergence ok.

--- src/KLLS.jl
+++ src/KLLS.jl
@@ -33,64 +33,84 @@ Evaluate logΣexp, its gradient, and Hessian at `p`:
 
 where `p` is a vector of length `n` and `lse` is a LogExpFunction object.
 
-This implementation safeguards against numerical under/overflow,
-and is based on the following code:
+This implementation safeguards against numerical under/overflow:
 
 https://github.com/baggepinnen/MonteCarloMeasurements.jl/blob/4f9b688d298157dc24a5b0a518d971221fbe15dd/src/resampling.jl#L10
 """
 function (lse::LogExpFunction)(p::Vector)
-    w, q = lse.w, lse.q
-    maxval, maxind = findmax(w)
-    g .= exp.(q .* (p .- maxval))
+    q, g = lse.q, lse.g
+    maxval, maxind = findmax(p)
+    @. g = q*exp(p - maxval)
     Σ = sum_all_but(g, maxind) # Σ = ∑gₑ-1
     f = log1p(Σ) + maxval
-    g = g ./ (Σ+1)
-    H = Diagonal
+    @. g = g / (Σ+1)
+    H = Diagonal(g) - g * g'
+    return f, g, H
 end
 
 function sum_all_but(w, i)
     w[i] -= 1
     s = sum(w)
     w[i] += 1
-    s
+    return s
 end
 
 """
 Newton's method for minimizing unconstrained optimization problems
 
-    min_x KL(x|x̄) + λ/2 ||y||² subj to Ax + λy = b
+    min_x KL(p|q) + λ/2 ||y||² subj to Ap + λq = b
     min_y logΣexp(A'y) - ⟨b, y⟩ +  λ/2 ||y||²
-
-where `f` is a convex function, `A` is an m-by-n matrix, `b` is an m-vector, and `λ` is a nonnegative scalar.
 
 # Input
 - `A` is an m-by-n matrix
 - `b` is an m-vector
-- `x̄` is an n-vector on the simplex (no check is performed)
-- `λ` is a nonnegative scalar (default: 0.0)
+- `q` is an n-vector of priors in the unit simplex
+- `λ` is a nonnegative scalar (default: 1e-6)
 - `y0` is the initial guess (default: zeros(m))
 - `optTol` is the relative tolerance for the gradient (default: 1e-6)
 - `max_iter` is the maximum number of iterations (default: 100)
+
+# Output
+- `(p, y)` approximate primal-dual pair
 """
 function newton_opt(
-    A::Matrix, b::Vector, x̄::Vector, λ::Real=0.0;
-    y0::Vector = zeros(size(A, 2)),
+    A::Matrix, b::Vector, q::Vector, λ::Real=1e-6;
+    y0::Vector = zeros(size(A, 1)),
     optTol::Real = 1e-6,
     max_iter::Int = 100)
 
-    f, g, H = evalobj(fgh!, z)
-    ϵ = optTol * (1 + norm(g)) # relative tolerance
+    y = copy(y0)
+    logΣexp = LogExpFunction(q)
+
+    evaldual(y) = begin
+        f, g, H = logΣexp(A'y)
+        dObj = f + 0.5λ * dot(y, y) - dot(b, y)
+        dGrd = A*g - b + λ*y 
+        dHes = A*H*A' + λ*I
+        return dObj, dGrd, dHes
+    end
+
+    dObj, dGrd, dHes = evaldual(y)
+    ϵ = optTol * (1 + norm(dGrd)) # relative tolerance
+
     for k ∈ 1:max_iter
-        @printf("%3d: %10.3e %10.3e %10.3e\n", k, f, norm(g), eigvals(H)[1])
-        norm(g) < ϵ && break
-        d = -(H \ g)
-        if dot(d, g) > 0
+        logger(k, dObj, dGrd, dHes) 
+        norm(dGrd) < ϵ && break
+        d = -(dHes \ dGrd)
+        if dot(d, dGrd) > 0
             error("no descent")
         end
-        @. z = z + d
-        f, g, H = evalobj(fgh!, z)
+        @. y = y + d
+        dObj, dGrd, dHes = evaldual(y)
     end
-    return z
+    return y
+end
+
+function logger(k, dObj, dGrd, dHes)
+    if k == 1
+       @printf("%4s: %11s %11s %11s\n", "iter", "obj", "|grd|", "|hes|")
+    end
+    @printf("%4d: %11.4e %11.4e %11.4e\n", k, dObj, norm(dGrd), eigvals(dHes)[1])
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,14 @@ using KLLS
 # end
 
 
-using LogExpFunctions: logsumexp
-using Zygote
+m, n = 20, 30
+q = fill(1/n, n)
+A = randn(m, n)
+b = A*q + 0.1*randn(m)
+λ = 1e-2
+
+
+newton_opt(A, b, q, λ);
+
+
 
