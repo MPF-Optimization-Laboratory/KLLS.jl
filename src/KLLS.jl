fb589e9083afd02d9459f6906896a1995690c04a
Michael Friedlander <michael@friedlander.io>
2024-04-06 11:14:50 -0700

    Add linesearch

--- src/KLLS.jl
+++ src/KLLS.jl
@@ -2,7 +2,8 @@ module KLLS
 
 using LinearAlgebra
 using Printf
-export newton_opt
+
+export newton_opt, Tracer
 
 struct LogExpFunction
     q::Vector{Float64}  # prior
@@ -10,20 +11,20 @@ struct LogExpFunction
 end
 
 """
-Constructor for the LogExpFunction struct with prior `q`. 
+Constructor for the LogExpFunction object.
 
-    LogExpFunction(q) -> LogExpFunction(q, similar(q))
+If an n-vector of priors `q` is available:
+
+    LogExpFunction(q)
+    
+If no prior is known, instead provide the dimension `n`:
+
+    LogExpFunction(n)
 """
 function LogExpFunction(q::Vector) 
     @assert all(ζ->ζ≥0, q) && sum(q) ≈ 1
     LogExpFunction(q, similar(q))
 end
-
-"""
-Constructor for the LogExpFunction struct with uniform prior.
-
-    LogExpFunction(n) -> LogExpFunction(fill(1/n, n))
-"""
 LogExpFunction(n::Int) = LogExpFunction(fill(1/n, n))
 
 """
@@ -77,40 +78,78 @@ function newton_opt(
     A::Matrix, b::Vector, q::Vector, λ::Real=1e-6;
     y0::Vector = zeros(size(A, 1)),
     optTol::Real = 1e-6,
-    max_iter::Int = 100)
+    max_iter::Int = 100,
+    μ::Real = 1e-4)
 
+    tracer = Tracer()
     y = copy(y0)
     logΣexp = LogExpFunction(q)
+    ls_its = 0
 
     evaldual(y) = begin
-        f, g, H = logΣexp(A'y)
+        f, p, H = logΣexp(A'y)
         dObj = f + 0.5λ * dot(y, y) - dot(b, y)
-        dGrd = A*g - b + λ*y 
+        dGrd = A*p + λ*y - b 
         dHes = A*H*A' + λ*I
-        return dObj, dGrd, dHes
+        return dObj, dGrd, dHes, p
     end
 
-    dObj, dGrd, dHes = evaldual(y)
-    ϵ = optTol * (1 + norm(dGrd)) # relative tolerance
+    dObj, dGrd, dHes, p = evaldual(y)
+    ϵ = optTol * (1 + norm(dGrd, Inf)) # relative tolerance
 
-    for k ∈ 1:max_iter
-        logger(k, dObj, dGrd, dHes) 
-        norm(dGrd) < ϵ && break
+    for k ∈ 0:max_iter
+
+        # Log and test for exit
+        logger!(tracer, k, dObj, dGrd, dHes, ls_its)
+        if norm(dGrd, Inf) < ϵ
+            break
+        end
+
+        # Newton step. Replace with a CG solver for large problems
         d = -(dHes \ dGrd)
         if dot(d, dGrd) > 0
             error("no descent")
         end
-        @. y = y + d
-        dObj, dGrd, dHes = evaldual(y)
+
+        # Line search
+        α, ls_its = armijo(y->evaldual(y)[1], dGrd, y, d, μ=μ)
+
+        # Update y and evaluate objective quantities
+        @. y = y + α*d
+        dObj, dGrd, dHes, p = evaldual(y)
+
     end
-    return y
+    return p, y, tracer
 end
 
-function logger(k, dObj, dGrd, dHes)
-    if k == 1
-       @printf("%4s: %11s %11s %11s\n", "iter", "obj", "|grd|", "|hes|")
+function armijo(f, ∇fx, x, d; μ=1e-4, α=1, ρ=0.5, maxits=10)
+    for k in 1:maxits
+       if f(x+α*d) < f(x) + μ*α*dot(∇fx,d)
+           return α, k
+       end
+       α *= ρ
     end
-    @printf("%4d: %11.4e %11.4e %11.4e\n", k, dObj, norm(dGrd), eigvals(dHes)[1])
+    error("backtracking linesearch failed")
+end
+
+function logger!(tracer, k, dObj, dGrd, dHes, ls_its)
+    nrmdGrd = norm(dGrd)
+    if k == 0
+       @printf("%4s: %11s %11s %8s\n", "iter", "obj", "|grd|", "line its")
+    end
+    @printf("%4d: %11.4e %11.4e %8d\n", k, dObj, nrmdGrd, ls_its)
+    push!(tracer, dObj, nrmdGrd)
+end
+
+struct Tracer
+    dObj::Vector{Float64} # dual objective
+    pResid::Vector{Float64} # primal residuals
+end
+Tracer() = Tracer(Float64[], Float64[])
+import Base: push!
+function push!(t::Tracer, dObj, nrmdGrd)
+    push!(t.dObj, dObj)
+    push!(t.pResid, nrmdGrd)
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,16 @@
 using KLLS
-# using Test
 
+# using Test
 # @testset "KLLS.jl" begin
 #     # Write your tests here.
 # end
 
 
-m, n = 20, 30
+m, n = 2, 3
 q = fill(1/n, n)
 A = randn(m, n)
 b = A*q + 0.1*randn(m)
 λ = 1e-2
-
 
 newton_opt(A, b, q, λ);
 
