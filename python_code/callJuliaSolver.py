8abf6a8ae7be4fefd1f890876403170ddb2685f9
Michael Friedlander <michael@friedlander.io>
2024-05-29 22:05:13 -0700

    Update julia-to-py example

--- python_code/callJuliaSolver.py
+++ python_code/callJuliaSolver.py
@@ -1,20 +1,44 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+# This installs the Julia solver and dependencies
+# IMPORTANT: requires the `juliacall` Python package, eg,
+# `pip install juliacall`
+# from the command line
 from juliacall import Main as jl
-
-matrix = np.random.randn(3, 4)
-x = np.random.dirichlet(np.ones(4), size=1)[0]
-rhs = np.dot(matrix, x)
-
 jl.seval("""
          import Pkg
-         Pkg.add(url=\"github://\")
+         Pkg.add(url=\"git@github.com:MPF-Optimization-Laboratory/KLLS.jl.git\")
          using KLLS
          newtoncg = KLLS.solve!
          """)
 
-A = jl.convert(jl.Matrix, matrix)
-b = jl.convert(jl.Vector, rhs)
+# Generate some data
+n = 100
+m = 200
+x0 = np.random.rand(n)
+x0 /= np.sum(x0)
+Anp = np.random.rand(m, n)
+bnp = Anp @ x0
 
+# Convert data to Julia
+A = jl.convert(jl.Matrix, Anp)
+b = jl.convert(jl.Vector, bnp)
+
+# Create an instance of the KLLSData struct and set the regularization parameter
 data = jl.KLLSData(A, b)
-p = jl.newtoncg(data)
+data.λ = 1e-4
 
+# Solve the problem. Solution is in p[0]
+p = jl.newtoncg(data, trace=True)
+
+# The solution is in p[0]
+x = p[0]
+
+# Plot the solution
+plt.plot(x)
+plt.show()
+
+# To see the solver output log, add `trace=True` to the `newtoncg` call
+p = jl.newtoncg(data, trace=True)
+print(p[3])
