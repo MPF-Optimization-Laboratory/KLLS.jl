e5774fbb6b70fd8fd39972402fba2cd968ead915
Michael Friedlander <michael@friedlander.io>
2024-10-09 13:59:31 -0700

    self-scale newton wip

--- src/nlsolve.jl
+++ src/nlsolve.jl
@@ -6,22 +6,60 @@ function nljprod!(Jyt, zα, yt, ss::SSModel)
     jprod_residual!(ss, yt, zα, Jyt)
 end
 
+struct NewtonEQ end
 
-# reset!(kl)
-# ff = NonlinearFunction(KLLS.nlresidual!; jvp=KLLS.nljprod!)
-# y0 = zeros(m)
-# yt0 = vcat(y0, 1.0)
-# prob = NonlinearProblem(ff, yt0, ss)
-# @suppress_err begin
-# sol = solve(
-#     prob,
-#     reltol=1e-6,
-#     abstol=1e-6,
-#     show_trace = Val(true),
-#     trace_level = TraceAll(),
-#     store_trace = Val(true),
-#     NewtonRaphson(
-#         linsolve = KrylovJL_MINRES(verbose=10, itmax=10),
-#        )
-#     )
-# end;
+function solve!(
+    ss::SSModel{T},
+    ::NewtonEQ;
+    y0 = begin
+        m = ss.kl.meta.nvar
+        zeros(T, m)
+    end,
+    t0 = one(T),
+    atol = DEFAULT_PRECISION(T),
+    rtol = DEFAULT_PRECISION(T),
+    logging=0,
+    max_time=30.0,
+    max_iter=1000,
+    trace::Bool = false,
+    kwargs...) where T
+    
+    reset!(ss) # reset counters
+    m = ss.kl.meta.nvar
+
+    tracer = DataFrame(iter=Int[], scale=T[], vpt=T[], norm∇d=T[], cgits=Int[], cgmsg=String[])
+   
+    # Setup the NonlinearSolve objects
+    nlf = NonlinearFunction(nlresidual!, jvp=nljprod!)
+    prob = NonlinearProblem(nlf, vcat(y0, t0), ss)
+    nlcache = init(
+                prob,
+                reltol=rtol,
+                abstol=atol,
+                show_trace = Val(false),
+                store_trace = Val(false),
+                NewtonRaphson(
+                    linesearch = RobustNonMonotoneLineSearch(),
+                    linsolve = KrylovJL_MINRES(verbose=0, itmax=50),
+                )
+            )
+    
+    for iter in 1:100
+        @suppress_err step!(nlcache)
+
+        y = @view nlcache.u[1:m]
+        t =       nlcache.u[end]  
+        ∇d = @view nlcache.fu[1:m]
+        vpt =      nlcache.fu[end]
+       
+        norm∇d = norm(∇d, Inf)
+        log_items = (iter, t, vpt, norm∇d, nlcache.descent_cache.lincache.lincache.cacheval.stats.niter, nlcache.descent_cache.lincache.lincache.cacheval.stats.status) 
+        trace && push!(tracer, log_items)
+        
+        if nlcache.retcode ≠ ReturnCode.Default
+            break
+        end
+    end
+    
+    return nlcache, tracer
+end
