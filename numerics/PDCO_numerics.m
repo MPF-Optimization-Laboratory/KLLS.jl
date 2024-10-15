acf8003b6495eefc83fb12cc2d6841a7e566438d
mattkingros <72038195+mattkingros@users.noreply.github.com>
2024-10-15 12:23:06 -0400

    PDCO matlab code fully Migrated
    Added PDCO_numerics.m and associated output, saved to .csv files
    
    *potential issue, meet to discuss diagonal scaling in UEG (d1 in pdco.m)

--- numerics/PDCO_numerics.m
+++ numerics/PDCO_numerics.m
@@ -1,23 +1,39 @@
-lambdas = logspace(-4, 2, 7)
+lambdas = logspace(-2, 2, 3);
+print = 0;
+delta = 1e-3;
+t = datetime;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+folder = strcat(string(month(t)),string(day(t)),'_',string(hour(t)),string(minute(t)));
+directory = fullfile("GitHub\KLLS.jl\numerics\outputs",folder);
+    
+if ~exist(directory, 'dir')
+       mkdir(directory)
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % UEG Test Problem
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+clear A b mu;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 data = load('data/synthetic-UEG_testproblem.mat');
 
 A = data.A;
-[m, n] = size(A);
 
 b = data.b_avg;
 b = b';
-x0 = data.x; % already normalized
-x0 = x0'; 
 mu = data.mu; % prior provided
 mu = double(mu'); % was single precision
+
+for lam = lambdas
+    [x, y, z, inform,tracer_UEG] = PDCO_KL(A, b, mu, lam, delta, print);
+    name = strcat('UEG_PDCO_lam',string(lam));
+    writematrix(tracer_UEG,strcat(fullfile(directory,name),".csv"))
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -25,15 +41,20 @@ mu = double(mu'); % was single precision
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-for lam = lambdas
-    [x, y, z, inform,tracer_UEG] = PDCO_KL(A, b, mu, lmda_test, delta, print);
-    t = datetime
-    name = strcat('PDCO_UEG',string(month(t)),string(day(t)),'_',string(hour(t)),string(minute(t)),'_',string(lam))
-    writematrix(tracer_UEG,fullfile("GitHub\KLLS.jl\numerics\outputs",name,".csv"))
-end
+clear A b mu;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 data = load('data/rho-meson_testproblem.mat');
 A = data.A;
+
 b = data.b_avg;
-x0 = data.x;
+b =  b';
 mu = data.mu; % prior provided
+mu = double(mu');
+
+for lam = lambdas
+    [x, y, z, inform,tracer_rho_meson] = PDCO_KL(A, b, mu, lam, delta, print);
+    name = strcat('PDCO_rho_meson_lam',string(lam));
+    writematrix(tracer_rho_meson,strcat(fullfile(directory,name),".csv"))
+end
