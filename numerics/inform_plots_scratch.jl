9e806c1bd162b735c0b637178c6926195d229685
mattkingros <72038195+mattkingros@users.noreply.github.com>
2024-10-19 12:31:59 -0400

    Update inform_plots_scratch.jl

--- numerics/inform_plots_scratch.jl
+++ numerics/inform_plots_scratch.jl
@@ -20,11 +20,14 @@ include("solve_metrics.jl")
 ##################################################
 
 laplace_dict = matread(joinpath(pwd(),"data","Laplace_inversion.mat"))
+laplace_dict= matread(joinpath(pwd(),"data","rho-meson_testproblem.mat"))
 
 A = laplace_dict["A"]
-b = vec(laplace_dict["b"])
-x_plot = vec(laplace_dict["x_values"])
-p_soln = vec(laplace_dict["p"])'
+b = vec(laplace_dict["b_avg"])
+x_test = vec(laplace_dict["x"])
+
+#x_plot = vec(laplace_dict["x_values"])
+#p_soln = vec(laplace_dict["p"])'
 
 μ = ones(size(A)[2]);
 μ .= max.(μ, 1e-13)
@@ -33,7 +36,7 @@ p_soln = vec(laplace_dict["p"])'
 optTol = 10e-12
 mas_iter =1000;
 max_time = 60.0;
-lambdas = (10.0).^(range(-6,stop=-1,length=6))
+lambdas = (10.0).^(range(-12,stop=-5,length=7))
 lambdas =round.(lambdas, sigdigits = 3)
 
 #################################################################################
@@ -44,16 +47,27 @@ lambdas =round.(lambdas, sigdigits = 3)
 
 
 figure_1 = plot(title = "Laplace Inversion", xlabel = "", ylabel = "")
-plot!(x_plot,p_soln',labels = "True Density")
+plot!(x_test,labels = "True Density")
+
 for λ in lambdas
     local data = KLLSModel(A,b,C=I, q = μ, λ = λ)
-    stats = KLLS.solve!(data, atol=optTol, rtol=optTol,max_iter = max_iter,trace=true)
-    plot!(x_plot,stats.solution, labels = "λ = " * string(λ))
+    stats = KLLS.solve!(data, atol=optTol, rtol=optTol,max_iter = max_iter,trace=true,logging=true)
+    plot!(stats.solution, labels = "λ = " * string(λ))
+
 end
 
-data = KLLSModel(A,b,C=I, q = μ, λ = λ)
 
-f(y) = KLLS.dObj!(data,y) # DUAL objective, dim y = dim b, duh
+display(figure_1)
+
+############################################################################
+#
+# figure 2
+#
+#############################################################################
+
+data = KLLSModel(A,b,C=I, q = μ, λ = 10e-8)
+
+f(y) = KLLS.dObj!(data,y) # DUAL objective, 
 grad!(grad_in_place,y) = KLLS.dGrad!(data,y,grad_in_place)
 z0 = zeros(size(data.A,1))
 
@@ -66,13 +80,12 @@ opt_sol =Optim.optimize(f,grad!,z0,
         #callback = cb,
         # Piggyback trace to return ∇f and f evals per iteration
         g_tol = optTol + optTol*data.bNrm,
-
         # This is something to chat about, early stopping criterion
         f_tol = 0,
         x_tol = 0
 )
 )
-
-primal_sol = KLLS.dGrad!(data,opt_sol.trace[end].metadata["x"]) + opt_sol.trace[end].metadata["g(x)"] + b
-
-display(figure_1)
+KLLS.obj!(data.lse,A'*opt_sol.minimizer)
+test = data.lse.g
+plot!(test)
+#primal_sol = KLLS.dGrad!(data,opt_sol.trace[end].metadata["x"]) + opt_sol.trace[end].metadata["g(x)"] + b
