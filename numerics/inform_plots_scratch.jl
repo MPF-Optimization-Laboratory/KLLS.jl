6e080d09e2c520319c14078eb5ae225b3b872318
mattkingros <72038195+mattkingros@users.noreply.github.com>
2024-10-19 13:59:16 -0400

    Laplace inversion plots replaced
    Laplace inversion plots replaced with UEG & rho-meson problems for the talk.
    
    During meeting with michael.

--- numerics/inform_plots_scratch.jl
+++ numerics/inform_plots_scratch.jl
@@ -19,14 +19,14 @@ include("solve_metrics.jl")
 #
 ##################################################
 
-laplace_dict = matread(joinpath(pwd(),"data","Laplace_inversion.mat"))
+#laplace_dict = matread(joinpath(pwd(),"data","Laplace_inversion_large.mat"))
 laplace_dict= matread(joinpath(pwd(),"data","rho-meson_testproblem.mat"))
 
 A = laplace_dict["A"]
 b = vec(laplace_dict["b_avg"])
 x_test = vec(laplace_dict["x"])
 
-# x_plot = vec(laplace_dict["x_values"])
+#x_plot = vec(laplace_dict["x_values"])
 #p_soln = vec(laplace_dict["p"])
 
 μ = ones(size(A)[2]);
@@ -34,20 +34,22 @@ x_test = vec(laplace_dict["x"])
 μ .= μ./sum(μ)
 
 optTol = 10e-12
-mas_iter =1000;
+max_iter =1000;
 max_time = 60.0;
-lambdas = (10.0).^(range(-12,stop=-5,length=7))
+lambdas = (10.0).^(range(-12,stop=-9,length=4))
 lambdas =round.(lambdas, sigdigits = 3)
+
+lamdas = [1.0]
 
 #################################################################################
 #
-# Figure 1
+# Figure 1 - ρ-meson
 #
 ##########################################################################
 
 
-figure_1 = plot(title = "Laplace Inversion", xlabel = "", ylabel = "")
-plot!(x_test,labels = "True Density")
+figure_1 = plot(title = "ρ-meson problem", xlabel = "ω (frequency)", ylabel = "Probability")
+plot!(x_test,labels = "True Density",lw = 2)
 
 for λ in lambdas
     local data = KLLSModel(A,b,C=I, q = μ, λ = λ)
@@ -61,13 +63,55 @@ display(figure_1)
 
 ############################################################################
 #
+# figure 2 - UEG 
+#
+############################################################################
+
+
+UEG_dict= matread(joinpath(pwd(),"data","synthetic-UEG_testproblem.mat"))
+
+A = UEG_dict["A"]
+b = vec(UEG_dict["b_avg"])
+x_test = vec(UEG_dict["x"])
+
+μ = vec(UEG_dict["mu"])
+μ .= max.(μ, 1e-13)
+μ .= μ./sum(μ)
+μ = convert(Vector{Float64},μ)
+
+C = vec(inv.(UEG_dict["b_std"]')) |> diagm
+
+optTol = 10e-12
+max_iter =1000;
+max_time = 60.0;
+lambdas = (10.0).^(range(-4,stop=-2,length=3))
+lambdas =round.(lambdas, sigdigits = 3)
+
+############################################################################
+
+
+figure_2 = plot(title = "UEG", xlabel = "", ylabel = "")
+plot!(x_test,labels = "True Density")
+
+for λ in lambdas
+    local data = KLLSModel(A,b,C=C,q=μ,λ=λ)
+    stats = KLLS.solve!(data, atol=optTol, rtol=optTol,max_iter = max_iter,trace=true,logging=true)
+    plot!(stats.solution, labels = "λ = " * string(λ))
+
+end
+
+
+display(figure_2)
+
+############################################################################
+#
 # figure 2
 #
 #############################################################################
 
 # One fixed λ to compare work between trunk / LBFGS / PDCO.
 # pick a small one, 10e-4
-
+#=
 data = KLLSModel(A,b,C=I, q = μ, λ = 10e-4)
 
 f(y) = KLLS.dObj!(data,y) # DUAL objective, 
@@ -90,3 +134,4 @@ opt_sol =Optim.optimize(f,grad!,z0,
 )
 
 Opt_sol.trace
+=#
